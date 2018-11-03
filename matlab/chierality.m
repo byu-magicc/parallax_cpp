9350fbcb39ee3282dd497f7b6b10af5302352099
Jacob White <snowflakeobsidian3.14@gmail.com>
2018-11-02 19:56:33 -0600

    Chierality checks are mostly working, but something seems fishy with the truth data.

--- matlab/chierality.m
+++ matlab/chierality.m
@@ -1,13 +1,14 @@
+clc
 video = 'holodeck';
-methods = {'poly_opencv', 'gn_eigen'};
-%methods = {'poly_opencv_trace', 'poly_opencv_chierality', 'poly_opencv_no_chierality'};
+%methods = {'poly_opencv', 'gn_eigen'};
+methods = {'poly_opencv', 'poly_opencv_trace', 'poly_opencv_no_chierality', 'gn_eigen', 'gn_eigen_no_chierality'};
 test = 'chierality';
 lgnd = cell(1, length(methods));
-fprintf('%-20s %-15s %-15s %-15s %-15s \n', 'Method', 'Rotation', 'Translation', 'Rot ratio', 'Tr ratio')
+fprintf('%-25s %-15s %-15s %-15s %-15s \n', 'Method', 'Rotation', 'Translation', 'Rot ratio', 'Tr ratio')
 for i = 1:length(methods)
 	filename = ['../logs/' video '/' methods{i} '/' test '/accuracy.bin'];
 	A = read_binary(filename, 4);
-	fprintf('%-20s %-15d %-15d %-15.3f %-15.3f\n', methods{i}, sum(A(3:4, :), 2).', mean(A(3:4, :), 2).')
+	fprintf('%-25s %-15d %-15d %-15.3f %-15.3f\n', methods{i}, sum(A(3:4, :), 2).', mean(A(3:4, :), 2).')
 	lgnd{i} = replace(methods{i}, '_', ' ');
 end
 
